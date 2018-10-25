9168a3e651d2b7a8b6d7dcf48390fe7c5457afa3
Jonathan <jonathan.fauchoux@gmail.com>
2018-10-25 15:13:08 +0200

    test background

--- ajax.js
+++ ajax.js
@@ -25,17 +25,20 @@ xhr.onload = function() {
 }
 
 xhr.send(); //Call send
-let body = document.querySelector("body");
+
 let randomBack = () =>{
+    let body = document.querySelector("#body");
     let random1 = Math.floor(Math.random()* 3)+1 ;
     if (random1 == 1){
-    body.style.background = url("https://images.ecosia.org/-3VZKOlzP83U-TTfrQ3Wve4tgJE=/0x390/smart/https%3A%2F%2Ft3.ftcdn.net%2Fjpg%2F00%2F57%2F54%2F34%2F240_F_57543467_z6bcsQ0jMSajakFxZyRsmZVYqJyk5yhu.jpg");
+        body.style.background ='url("https://images.ecosia.org/-3VZKOlzP83U-TTfrQ3Wve4tgJE=/0x390/smart/https%3A%2F%2Ft3.ftcdn.net%2Fjpg%2F00%2F57%2F54%2F34%2F240_F_57543467_z6bcsQ0jMSajakFxZyRsmZVYqJyk5yhu.jpg")';
     } else if (random1 == 2){
-        body.style.background = url("https://images.ecosia.org/QGn330npsfb25hkfuydDiboFe9g=/0x390/smart/http%3A%2F%2Fst.gde-fon.com%2Fwallpapers_original%2F130243_zhyoltye_lepestki_zelyonoe_nasekomoe_glaza_fokus_f_1920x1285_www.Gde-Fon.com.jpg");
+        body.style.background = 'url("https://images.ecosia.org/QGn330npsfb25hkfuydDiboFe9g=/0x390/smart/http%3A%2F%2Fst.gde-fon.com%2Fwallpapers_original%2F130243_zhyoltye_lepestki_zelyonoe_nasekomoe_glaza_fokus_f_1920x1285_www.Gde-Fon.com.jpg")';
     }else{
-        body.style.background = url("https://wonderfulengineering.com/wp-content/uploads/2014/09/Green-Wallpaper-9.jpg");
+        body.style.background ='url("https://wonderfulengineering.com/wp-content/uploads/2014/09/Green-Wallpaper-9.jpg")';
     }
 }
+randomBack();
+console.log(random1)
 
 
 
